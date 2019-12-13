2a0ab635352f8527f5fad122d2c9ad5ae955928e
chenyafeng <chenyafeng@whaty.com>
2019-12-13 16:19:33 +0800

    添加获取单个用户的controller方法

--- module-user-controller/src/main/java/com/windf/study/springcloud/user/controller/UserController.java
+++ module-user-controller/src/main/java/com/windf/study/springcloud/user/controller/UserController.java
@@ -4,10 +4,7 @@ package com.windf.study.springcloud.user.controller;
 import com.windf.study.springcloud.user.domain.User;
 import com.windf.study.springcloud.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -28,6 +25,11 @@ public class UserController {
     @GetMapping("/users")
     public Collection<User> list() {
         return userService.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
 }
