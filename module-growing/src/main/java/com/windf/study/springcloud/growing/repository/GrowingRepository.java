d0c26c1ec58b6b3807e9ebb0d3dacbcd3b514b3d
windfChen <q3400p@sina.cn>
2019-12-21 16:43:39 +0800

    功能：日志存储名字修改

--- module-growing/src/main/java/com/windf/study/springcloud/growing/repository/GrowingRepository.java
+++ module-growing/src/main/java/com/windf/study/springcloud/growing/repository/GrowingRepository.java
@@ -2,7 +2,7 @@ package com.windf.study.springcloud.growing.repository;
 
 import com.windf.study.springcloud.growing.domain.Log;
 
-public interface UserRepository {
+public interface GrowingRepository {
     /**
      * 持久化存储日志
      * @param log
