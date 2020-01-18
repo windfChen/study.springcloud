3a39a6072e881fee6facc5198e851ba9fa42deb2
windfChen <q3400p@sina.cn>
2020-01-18 16:13:30 +0800

    功能：添加根据用户名获取用户的方法

--- module-user/src/main/java/com/windf/study/springcloud/user/repository/UserRepository.java
+++ module-user/src/main/java/com/windf/study/springcloud/user/repository/UserRepository.java
@@ -5,9 +5,30 @@ import com.windf.study.springcloud.user.entity.User;
 import java.util.Collection;
 
 public interface UserRepository {
+    /**
+     * 添加用户
+     * @param user
+     * @return
+     */
     boolean save(User user);
 
+    /**
+     * 获取所有用户
+     * @return
+     */
     Collection<User> findAll();
 
-    User getUserById(Long id);
+    /**
+     * 根据id获取用户
+     * @param id
+     * @return
+     */
+    User getUserById(String id);
+
+    /**
+     * 根据用户名获取用户
+     * @param loginId
+     * @return
+     */
+    User getByUsername(String loginId);
 }
